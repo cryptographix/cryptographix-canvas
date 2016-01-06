5ece4951336758dc7fe21c17a6987a7a6815cbba
Sean Michael Wykes <sean.wykes@nascent.com.br>
2016-01-06 13:34:01 -0200

    created from package-template

---
+++ config.js
@@ -0,0 +1,9 @@
+System.config({
+  defaultJSExtensions: true,
+  transpiler: "none",
+  paths: {
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*",
+    "cryptographix-canvas": "dist/system/cryptographix-canvas.js"
+  }
+});
